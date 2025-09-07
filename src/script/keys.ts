ef13b3da2782628921314e4f65ff5db653d2cd2e
Oleshkevich Alexander <araneusX@gmail.com>
2025-09-07 12:22:46 +0300

    Update keys.ts

--- src/script/keys.ts
+++ src/script/keys.ts
@@ -1,5 +1,5 @@
 export const IPGEOLOCATION_KEY = '24e4ff2dc399413d995ec800beb374dc';
-export const OPENCAGEDATA_KEY = '1419ac583fc6419ab36dff373362a387';
+export const OPENCAGEDATA_KEY = '6bc180117a334fa6a2accbeb1d09fa09';
 // export const UNSPLASH_KEY = 'pyapfVKVRdQ3L7Bi3KvCfSo-gZLdddg7mGvyrYhN3cI';
 export const WEATHERAPI_KEY = '2c3bed38cd2140c2be9225108200106';
 export const FLICKR_KEY = 'ef2f7201baf9c884ff066f2f278d5ce2';
